77a7b966b20a833a6974fc6534778debb282586c
AkshayRJ28 <118600264+AkshayRJ28@users.noreply.github.com>
2022-12-11 13:40:10 +0530

    loops

---
+++ Cwh_21_loops_chp5.java
@@ -0,0 +1,24 @@
+package Practice;
+
+public class Cwh_21_loops_chp5 {
+
+	public static void main(String[] args) {
+		int i=1;
+		while(i<5)
+		{
+			System.out.println(i);
+			i++;
+		}
+		System.out.println("while loop is end");
+		
+		// do while loop 
+		int b=0;
+		do{
+			System.out.println(b);
+			b++;
+		}
+		while(b<5);
+		
+	}
+
+}
