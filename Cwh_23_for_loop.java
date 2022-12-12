1130cb8d244e6f2b6613c9b30af6b2e943e21ba3
AkshayRJ28 <118600264+AkshayRJ28@users.noreply.github.com>
2022-12-12 17:41:59 +0530

    for loop

---
+++ Cwh_23_for_loop.java
@@ -0,0 +1,27 @@
+package Practice;
+
+public class Cwh_23_for_loop {
+
+	public static void main(String[] args) {
+
+		for(int a=0;a<=10;a++)
+		{
+			System.out.println(a);
+		}
+		System.out.println("End for loop successfully");
+		//wap print first n odd numbers
+		
+		for (int b =0; b<=10;b++)
+		{
+			{
+				
+				//2n = Even number = 2,4,6
+				//2n+1= odd number =1,3,5
+			
+			 System.out.println(2*b+1);
+				
+			}
+		}
+	}
+
+}
